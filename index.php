c3eb45561104a343c64d9170551ab296b2a2f7c8
Bimsara Jayasena <129860594+bimsara-jayasena@users.noreply.github.com>
2024-10-25 21:48:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require '../Config/DbUtil.php';
+require './Config/DbUtil.php';
 $connection=new DbUtil();
 $connection->getConnection();
 echo "you are connected to the databases susccessfully!"
